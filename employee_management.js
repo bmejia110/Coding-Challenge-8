a977339e211cb6120b93caf303ca31fa8fa80297
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-06 14:47:09 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -11,3 +11,19 @@ class Employee {
         return "${this._name works as ${this._position} in ${this._department}, eanring $${this._salary}.";
     }
 }
+
+//Task 2: Create Department Class
+
+class Department {
+    constructor(name) {  //constuctor for department properties
+        this._name = name;
+        this._employees = [];  //array to hold employee objects
+    }
+    addEmployee(employee) {
+        this._employees.push(employee);  //add employee object to array
+    }
+
+    getDepartmentSalary () {
+        return this._ this._employees.reduce((total, employee) => total + employee._salary, 0);  // use reduce to sump up salaries
+    }
+}
